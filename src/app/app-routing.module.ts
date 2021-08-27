8d52edfa3d2845eedc461c0e390ed5869b28bbcd
Ravi Raj <ravi.raj@corp.vcti.io>
2021-08-27 13:40:09 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { OffersComponent } from './components/offers/offers.component'; 
+import { MobilesComponent } from './components/mobiles/mobiles.component';
+const routes: Routes = [
+  {path : '', component:HomeComponent},
+  {path : 'offer', component:OffersComponent},
+  {path : 'mobiles', component:MobilesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { OffersComponent } from './components/offers/offers.component';
+import { MobilesComponent } from './components/mobiles/mobiles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OffersComponent,
+    MobilesComponent
   ],
   imports: [
     BrowserModule,
